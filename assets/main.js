f43c978b87d3bd45903c151d231f6078373560ed
Jack Matier <self@jackalyst.com>
2018-03-10 19:55:39 -0700

    Mobile fixes + logo

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,39 @@
 
 $(document).ready(function() {
+    function hasTouch() {
+        return 'ontouchstart' in document.documentElement
+               || navigator.maxTouchPoints > 0
+               || navigator.msMaxTouchPoints > 0;
+    }
+
+    if (hasTouch()) { // remove all :hover stylesheets
+        try { // prevent exception on browsers not supporting DOM styleSheets properly
+            for (var si in document.styleSheets) {
+                var styleSheet = document.styleSheets[si];
+                if (!styleSheet.rules) continue;
+
+                for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                    if (!styleSheet.rules[ri].selectorText) continue;
+
+                    if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                        styleSheet.deleteRule(ri);
+                    }
+                }
+            }
+        } catch (ex) {}
+    }
+
     $('#nav-icon').click(function(){
         $(this).toggleClass('open');
     });
     // Click
-    $('.has-child > a').on('click', function(event) {
+    $('.has-child > a, .dropdown > a').on('click', function(event) {
       event.preventDefault();
 
+      $(this).parent().siblings().removeClass('active');
       $(this).parent().toggleClass('active');
-    });
 
+    });
  
     var sections = $('.document-page h2, .document-page h3'), nav = $('.floatmenu nav ul'), nav_height = nav.outerHeight();
      
@@ -24,15 +48,5 @@ $(document).ready(function() {
             return false;
         }
       });
-    });
-
-
-    $('.button-linux').on('click', function() {
-      $('body').removeClass('windows');
-      $('body').addClass('linux');
-    });
-    $('.button-windows').on('click', function() {
-      $('body').removeClass('linux');
-      $('body').addClass('windows');
     });
 });
