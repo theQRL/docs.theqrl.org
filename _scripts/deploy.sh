ee4003b611c10bf380dbad1d7b9ac5c07f9c9ef6
JP Lomas <jp.lomas@gmail.com>
2018-03-11 21:06:31 +0000

    Update deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,11 +3,11 @@
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
     ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf $TARGET_DIR
-    mkdir $TARGET_DIR
+    rm -rf "$TARGET_DIR"
+    mkdir "$TARGET_DIR"
     quit
 EOF
 
     cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" $TARGET_DIR .
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" "$TARGET_DIR" .
 fi
